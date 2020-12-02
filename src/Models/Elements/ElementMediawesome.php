86f64e9147d34f1afd5ca350bb6b0d53304b55e9
Mark Taylor <tardinha@gmail.com>
2020-12-02 12:59:47 +1100

    Initial commit

---
+++ src/Models/Elements/ElementMediawesome.php
@@ -0,0 +1,91 @@
+<?php
+namespace NSWDPC\Elemental\Models\Mediawesome;
+
+use DNADesign\Elemental\Models\ElementContent;
+use nglasl\mediawesome\MediaPage;
+use nglasl\mediawesome\MediaHolder;
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\NumericField;
+
+/**
+ * ElementMediawesome adds a featured video
+ */
+class ElementMediawesome extends ElementContent {
+
+    private static $inline_editable = false;
+
+    private static $icon = 'font-icon-picture';
+
+    private static $table_name = 'ElementMediawesome';
+
+    private static $title = 'Mediawesome list';
+    private static $description = "Display a list of Mediawesome items";
+
+    private static $singular_name = 'Mediawesome';
+    private static $plural_name = 'Mediawesomes';
+
+    public function getType()
+    {
+        return _t(__CLASS__ . '.BlockType', 'Mediawesome list');
+    }
+
+    private static $db = [
+        'NumberOfPosts' => 'Int'
+
+    ];
+
+    private static $defaults = [
+        'NumberOfPosts' => 4
+    ];
+
+    private static $has_one = [
+        'MediaHolder' => MediaHolder::class
+    ];
+
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function($fields)
+        {
+                $fields->removeByName(['MediaHolderID']);
+
+                $fields->addFieldsToTab(
+                    'Root.Main', [
+                        DropdownField::create(
+                            'MediaHolderID',
+                            _t(
+                                __CLASS__ . 'HOLDER_ID', 'Choose a media holder'
+                            ),
+                            $this->getMediaHolders()
+                        ),
+                        NumericField::create(
+                            'NumberOfPosts',
+                            _t(
+                                __CLASS__ . 'POSTS', 'Number of Posts'
+                            )
+                        )
+                    ]
+                );
+
+            });
+        return parent::getCMSFields();
+    }
+
+    public function getMediaHolders() {
+        return MediaHolder::get();
+    }
+
+    public function getRecentPosts()
+    {
+        $mediaHolder = $this->MediaHolder();
+        $mediaPages = MediaPage::get()->sort('Date', 'DESC')->filter('ParentID', $mediaHolder->ID);
+
+        if ($mediaPages)
+        {
+          return $mediaPages->limit($this->NumberOfPosts);
+        }
+
+        return null;
+    }
+
+}
---
+++ templates/NSWDPC/Elemental/Models/Mediawesome/ElementMediawesome.ss
@@ -0,0 +1,23 @@
+<div class="{$ElementStyles}">
+    <div class="featured-video-element__content">
+	    <% if $ShowTitle %>
+            <h2 class="content-element__title">{$Title.XML}</h2>
+        <% end_if %>
+	   $HTML
+        <ul class="media-list media-release-list">
+            <% loop $RecentPosts %>
+                <li class="media $FirstLast">
+                <div class="media-body">
+                    <h4 class="media-heading"><a href="$Link" title="More information about $Title">$MenuTitle.XML</a></h4>
+                    <p class="small">$Date.Full</p>
+                </div>
+            </li>
+            <% end_loop %>
+        </ul>
+        <p class="more">
+            <a title="View more news" href="$MediaHolder.Link">
+                View more news <span aria-hidden="true" class="fa fa-chevron-right"></span>
+            </a>
+        </p>
+    </div>
+</div>
